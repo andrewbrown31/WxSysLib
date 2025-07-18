c38e478d6ff320ae6f36f0b2e4b974798365b222
Michael Barnes <mb0427@gadi-login-08.gadi.nci.org.au>
2025-07-18 11:17:46 +1000

    readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'WxSysLib'
+copyright = '2025, 21st Century Weather'
+version = '1.0'
+release = '1.0.0'
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
