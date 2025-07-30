055250f9575eef59051a4895d29771a0bc25aad5
Michael Barnes <39979861+weathermanbarnes@users.noreply.github.com>
2025-07-31 09:45:31 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,8 +3,19 @@ copyright = '2025, 21st Century Weather'
 version = '1.0'
 release = '1.0.0'
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon', # For Google/NumPy style docstrings
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.viewcode',
+]
+# The root document.
+root_doc = 'index'
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+html_theme = 'sphinx_rtd_theme' # Or another theme like 'pydata_sphinx_theme'
 
