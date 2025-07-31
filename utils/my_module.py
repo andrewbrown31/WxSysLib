fc9a842c63db49da9e8726e801161390cd33886b
Michael Barnes <mb0427@gadi-login-02.gadi.nci.org.au>
2025-07-31 14:34:58 +1000

    Test for readthedocs

---
+++ utils/my_module.py
@@ -0,0 +1,16 @@
+def my_function(param1, param2):
+    """
+    This is a short description of my_function.
+
+    A more detailed explanation can go here.
+
+    :param param1: Description of the first parameter.
+    :type param1: int
+    :param param2: Description of the second parameter.
+    :type param2: str
+    :returns: A description of what the function returns.
+    :rtype: bool
+    :raises ValueError: if param1 is negative.
+    """
+    # ... function implementation
+    return True
