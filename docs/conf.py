7146f15c5c69bee328295dfb24d2efab02b53682
Michael Barnes <39979861+weathermanbarnes@users.noreply.github.com>
2025-07-31 11:31:59 +1000

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,12 +3,26 @@ copyright = '2025, 21st Century Weather'
 version = '1.0'
 release = '1.0.0'
 
+#extensions = [
+#    'sphinx.ext.autodoc',
+#    'sphinx.ext.napoleon', # For Google/NumPy style docstrings
+#    'sphinx.ext.intersphinx',
+#    'sphinx.ext.viewcode',
+#]
+
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon', # For Google/NumPy style docstrings
+    'sphinx.ext.napoleon',
+    'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinx_rtd_theme', # Often used with Read the Docs
 ]
+
 # The root document.
 root_doc = 'index'
 
