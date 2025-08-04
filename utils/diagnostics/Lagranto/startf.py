79d63e877a63a07a2f9cebf7dc640f9f99bfb1e3
Chenhui <chenhui.jin1@monash.edu>
2025-08-04 16:33:05 +1000

    Update startf.py

--- utils/diagnostics/Lagranto/startf.py
+++ utils/diagnostics/Lagranto/startf.py
@@ -26,21 +26,3 @@ def create_starf_line(statdir, lon_sta, lon_end, lat_sta, lat_end, interval, lev
          for lon, lat in zip(lons, lats):
             starf_line.write('0.00 '+ str(lon).rjust(2,'0') + ' ' + str(lat).rjust(2,'0') + ' '+str(lev)+'\n')
             s = s + 1
-
-
-def create_circ_starf_circle(statdir, clon, clat, max_radius_km, spacing_km, lev_range):
-	starf_box = open(f'{statdir}', 'w')
-	s = 0
-	n_rings = int(max_radius_km//spacing_km)
-	for lev in lev_range:
-		for i in range(n_rings + 1):
-			r = i * spacing_km
-			if r == 0:
-				starf_box.write(f"0.00 {clon:.2f} {clat:.2f} {lev}\n")
-				s += 1
-			else:
-				n_points = max(6, int(round(2 * np.pi * r / spacing_km)))
-				for angle in np.linspace(0, 360, n_points, endpoint=False):
-					dest = geodesic(kilometers=r).destination((clat, clon), angle)
-					starf_box.write(f"0.00 {dest.longitude:.2f} {dest.latitude:.2f} {lev}\n")
-					s += 1
